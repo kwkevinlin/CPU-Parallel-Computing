eb58aacdbf3a808fa490bc1bc920859b6cb08790
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-02-29 19:58:15 -0500

    Removed storageDim declaration, added clusterElement count printout

--- (Lab 1) KMeans Clustering with Pthreads/main.c
+++ (Lab 1) KMeans Clustering with Pthreads/main.c
@@ -84,8 +84,6 @@ int main(int argc, char* argv[]) {
 	//For step 4
 	int clusterElements[K];
 	memset(clusterElements, 0, sizeof(clusterElements));
-	double storageDim[dimensions];
-	memset(storageDim, 0, sizeof(dimensions));
 
 	/*==========================================================================
 		Major loop starts here
@@ -113,27 +111,19 @@ int main(int argc, char* argv[]) {
 			printf("Sample %i closest cluster: %i\n", i, dataClusterIndex[i]);
 			clusterElements[dataClusterIndex[i]]++;
 			minDist = 100000;
-			//Update closest cluster array
-			/*
-			for (int y = 0; y < dimensions; y++) {
-				clusters[minCluster][clusterIndex[minCluster]][y] = data[i][y];
-				clusters[minCluster][clusterIndex[minCluster]][y] = data[i][y];
-			}
-			clusterIndex[minCluster]++;
-			*/
 		}
 
-		//4. Move the center of each cluster to be in the middle of the elements that are assigned to that cluster.
-		//Get average of coordinates. x+x+x/n, y+y+y/n
+		
 		//memset(clusterInfo, 0, sizeof(double) * K * dimensions);
-
 		for (i = 0; i < K; i++) {
+			printf("Cluster %i contains: %i elements\n", K, clusterElements[i]);
 			for (j = 0; j < dimensions; j++) {
 				clusterInfo[i][j] = 0;
 			}
 		}
 
-
+		//4. Move the center of each cluster to be in the middle of the elements that are assigned to that cluster.
+		//Get average of coordinates. x+x+x/n, y+y+y/n
 		for (i = 0; i < K; i++) { //Per Cluster
 			printf("Cluster %i (avg): ", i);
 			for (j = 0; j < samples; j++) { //For each sample in each cluster
