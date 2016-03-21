2ca696b3ee319da8ce41cf1432791ba59dbd0601
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-03-21 00:59:47 -0400

    Almost done except output ordering and write to file

---
+++ (Lab 2) Counting Motifs in Sequences with MPI/sequential.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+	//	if (argc != 4) {
+	//		cout << "Incorrect number of arguments. Terminating.\n";
+	//		exit(-1);
+	//	}
+
+	// ./motifsearch motifFilename sequenceFilename outputFilename
+	ifstream inMotif("classMotifs.txt");
+	ifstream inSequence("classSequences.txt");
+	ofstream output("outputSmall.txt");
+
+	vector<string> motifs;
+	vector<string> sequences;
+
+	string n;
+	inMotif >> n >> n;
+	while (inMotif >> n) {
+		motifs.push_back(n);
+	}
+	inSequence >> n >> n;
+	while (inSequence >> n) {
+		sequences.push_back(n);
+	}
+
+	for (int i = 0; i < motifs.size(); i++) {
+		cout << motifs[i] << " ";
+	} cout << endl;
+	for (int i = 0; i < sequences.size(); i++) {
+		cout << sequences[i] << " ";
+	} cout << endl << endl;
+
+	int isMatch = 1;
+	unordered_map<string, int> matchedMotifs; //Stores matched motifs and their count in a hash
+
+	//Brute force method
+	for (int i = 0; i < motifs.size(); i++) { //For every motif string
+		for (int j = 0; j < sequences.size(); j++) { //For every sequence string
+
+			//Compare character by character
+			for (int k = 0; k < motifs[i].length(); k++) {
+
+				if (motifs[i][k] != sequences[j][k] && motifs[i][k] != 'X') {
+					isMatch = 0;
+					break;
+				}
+			}
+
+			if (isMatch == 1) {
+				cout << "Match: " << motifs[i] << " and " << sequences[j] << endl;
+				matchedMotifs[motifs[i]]++;
+			} else {
+				isMatch = 1;
+			}
+
+		}
+	}
+
+	cout << endl << matchedMotifs.size() << endl;
+	for (auto kv : matchedMotifs) {
+		cout << kv.first << "," << kv.second << endl;
+	}
+
+}
