b5507c49e3affbd3238e0dd14c5f5e8fa040bc13
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-04-01 18:39:11 -0400

    Fixed out of bounds int index in histoCounter

--- (Lab 2) Counting Motifs in Sequences with MPI/Parallel_Motif_MPI.cpp
+++ (Lab 2) Counting Motifs in Sequences with MPI/Parallel_Motif_MPI.cpp
@@ -57,14 +57,23 @@ int main(int argc, char* argv []) {
 		// ifstream inSequence(argv[2]);
 		// ofstream output(argv[3]);
 
-		ifstream inMotif("motifsSmall.txt");
-		ifstream inSequence("sequencesSmall.txt");
-		ofstream output("outputSmall.txt");
+		// ifstream inMotif("motifsSmall.txt");
+		// ifstream inSequence("sequencesSmall.txt");
+		// ofstream output("outputSmall.txt");
+
+		ifstream inMotif("motifsMedium.txt");
+		ifstream inSequence("sequencesMedium.txt");
+		ofstream output("outputMedium.txt");
+
+		// ifstream inMotif("motifsLarge.txt");
+		// ifstream inSequence("sequencesLarge.txt");
+		// ofstream output("outputLarge.txt");
 
 		//Reading in Motifs
 		inMotif >> numMotifs >> motifsLength;
-		char motifs[(numMotifs * motifsLength) + 1]; //1 for \0
-		memset(motifs, '\0', sizeof(char)*((numMotifs * motifsLength) + 1));
+
+		char* motifs = (char*) malloc(sizeof(char) * ((numMotifs * motifsLength) + 1));
+		memset(motifs, '\0', sizeof(char) * ((numMotifs * motifsLength) + 1));
 		for (int i = 0; i < numMotifs; i++) {
 			inMotif >> (motifs + strlen(motifs)); //Read in, append to motifs
 			//strlen gives length to \0, so pointer arithmetic, starting from \0, append
@@ -80,9 +89,8 @@ int main(int argc, char* argv []) {
 
 		//Reading in Sequences
 		inSequence >> numSequences >> motifsLength; //n can be discarded since n will == motifsLength
-		//char sequences[(numSequences * motifsLength) + 1]; 
-		sequences = (char *) malloc(sizeof(char) * (numSequences * motifsLength) + 1); //sequenceLength == motifsLength
-		memset(sequences, '\0', sizeof(char)*((numSequences * motifsLength) + 1));
+		sequences = (char *) malloc(sizeof(char) * ((numSequences * motifsLength) + 1)); //sequenceLength == motifsLength
+		memset(sequences, '\0', sizeof(char) * ((numSequences * motifsLength) + 1));
 		for (int i = 0; i < numSequences; i++) {
 			inSequence >> (sequences + strlen(sequences));
 		}
@@ -105,16 +113,16 @@ int main(int argc, char* argv []) {
 		MPI_Bcast(&numSequences, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
 		//Distribute motifs
-		localMotif = (char *) malloc(sizeof(char) * (numMotifs/comm_sz) * motifsLength + 1);
-		MPI_Scatter(motifs, motifsLength, MPI_CHAR, localMotif, motifsLength, MPI_CHAR, 0, MPI_COMM_WORLD);
+		localMotif = (char *) malloc(sizeof(char) * ((numMotifs/comm_sz) * motifsLength + 1));
+		MPI_Scatter(motifs, (numMotifs/comm_sz) * motifsLength, MPI_CHAR, localMotif, (numMotifs/comm_sz) * motifsLength, MPI_CHAR, 0, MPI_COMM_WORLD);
 		
 		//Broadcast whole sequence
 		MPI_Bcast(sequences, (numSequences * motifsLength) + 1, MPI_CHAR, 0, MPI_COMM_WORLD);
 
 		//Two arrays to mimick hash table
-		matchedMotifs = (char*) malloc(sizeof(char) * (numMotifs/comm_sz) * motifsLength + 1);
+		matchedMotifs = (char*) malloc(sizeof(char) * ((numMotifs/comm_sz) * motifsLength + 1));
 		matchedCounter = (int*) malloc(sizeof(int) * numMotifs/comm_sz);
-		memset(matchedMotifs, '\0', sizeof(char) * (numMotifs/comm_sz) * motifsLength + 1);
+		memset(matchedMotifs, '\0', sizeof(char) * ((numMotifs/comm_sz) * motifsLength + 1));
 		memset(matchedCounter, 0, sizeof(int) * (numMotifs/comm_sz));
 
 		//Compare algorithm
@@ -146,10 +154,10 @@ int main(int argc, char* argv []) {
 
 		//Receive results from all other processes
 		//Matched Motifs
-		memset(motifs, '\0', sizeof(char)*((numMotifs * motifsLength) + 1)); //Store retrieve in motifs
+		memset(motifs, '\0', sizeof(char) * ((numMotifs * motifsLength) + 1)); //Store retrieve in motifs
 		MPI_Gather(matchedMotifs, (numMotifs/comm_sz) * motifsLength, MPI_CHAR, motifs, (numMotifs/comm_sz) * motifsLength, MPI_CHAR, 0, MPI_COMM_WORLD);
 		//Number of matches for each matched motifs (above)
-		int* histoCounter = (int*) malloc(sizeof(int) * (numMotifs/comm_sz));
+		int* histoCounter = (int*) malloc(sizeof(int) * (strlen(motifs)/motifsLength + 1));
 		memset(histoCounter, 0, sizeof(int)*(numMotifs/comm_sz));
 		MPI_Gather(matchedCounter, (numMotifs/comm_sz), MPI_INT, histoCounter, (numMotifs/comm_sz), MPI_INT, 0, MPI_COMM_WORLD);
 
@@ -175,15 +183,15 @@ int main(int argc, char* argv []) {
 				output << "," << histoCounter[index] << endl;
 				index++;
 			}
-			if (i < strlen(motifs)) {
+			if (i != strlen(motifs)) {
 				output << motifs[i];
 			}
 		}
 
 		output.close();
-
+		
 		free(histoCounter);
-
+		
 	} else { //Other processes
 
 		//Receive motifsLength, numMotifs, and numSequences
@@ -192,17 +200,17 @@ int main(int argc, char* argv []) {
 		MPI_Bcast(&numSequences, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
 		//Receive motifs
-		localMotif = (char *) malloc(sizeof(char) * (numMotifs/comm_sz) * motifsLength + 1);
-		MPI_Scatter(NULL, motifsLength, MPI_CHAR, localMotif, motifsLength, MPI_CHAR, 0, MPI_COMM_WORLD);
+		localMotif = (char *) malloc(sizeof(char) * ((numMotifs/comm_sz) * motifsLength + 1));
+		MPI_Scatter(NULL, (numMotifs/comm_sz) * motifsLength, MPI_CHAR, localMotif, (numMotifs/comm_sz) * motifsLength, MPI_CHAR, 0, MPI_COMM_WORLD);
 
 		//Receive whole sequence
-		sequences = (char *) malloc(sizeof(char) * (numSequences * motifsLength) + 1);
+		sequences = (char *) malloc(sizeof(char) * ((numSequences * motifsLength) + 1));
 		MPI_Bcast(sequences, (numSequences * motifsLength) + 1, MPI_CHAR, 0, MPI_COMM_WORLD);
 
 		//Two arrays to mimick hash table
-		matchedMotifs = (char*) malloc(sizeof(char) * (numMotifs/comm_sz) * motifsLength + 1);
+		matchedMotifs = (char*) malloc(sizeof(char) * ((numMotifs/comm_sz) * motifsLength + 1));
 		matchedCounter = (int*) malloc(sizeof(int) * (numMotifs/comm_sz));
-		memset(matchedMotifs, '\0', sizeof(char) * (numMotifs/comm_sz) * motifsLength + 1);
+		memset(matchedMotifs, '\0', sizeof(char) * ((numMotifs/comm_sz) * motifsLength + 1));
 		memset(matchedCounter, 0, sizeof(int) * (numMotifs/comm_sz));
 
 		//Compare algorithm
